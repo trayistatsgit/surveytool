a9918227e05aa0f23641c471b5078b1ce0f03aeb
TS-Learn <learning@trayistats.com>
2024-09-23 12:37:11 +0530

    newpage design
    new page design

--- src/pages/createSurvey/Partials/NewPage.tsx
+++ src/pages/createSurvey/Partials/NewPage.tsx
@@ -6,14 +6,13 @@ const NewPage = () => {
     const [clones, setClones] = useState<number[]>([]);
 
     const addClone = () => { 
-        // Add a new clone by incrementing the length of the current clones
         setClones([...clones, clones.length]); 
     }; 
 
     return (
         <div>
             <div className='container-newPage' onClick={addClone}>
-                <div className='logo'><span className='logo-color'>+</span></div>
+                <div className='logo-newPage'><span className='logo-color'>+</span></div>
                 <p className='newPage-text'>NEW PAGE</p>
             </div>
             <div>
