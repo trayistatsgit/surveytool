7083a1402426892848a1b21bb9faa33b45fb255f
TS-Learn <learning@trayistats.com>
2024-10-15 23:55:44 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import {RouterProvider } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
+// import AppRoutes from './routes/Routes';
+import { RouterProvider } from 'react-router-dom';
 import router from './routes/Routes';
 
 const App = () => {
 	return (
 		<>
-			<RouterProvider router={router} />
+			<Provider store={store}>
+				<PersistGate loading={null} persistor={persistor}>
+					<RouterProvider router={router} />
+				</PersistGate>
+			</Provider>
 		</>
 	);
 };
